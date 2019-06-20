493e32fdd3dbe8bb26319852a3ae369b7ebc3045
David Smith <25447681+davidcozsmith@users.noreply.github.com>
2019-06-19 22:56:50 -0700

    tests are broken, not sure why

--- test/GameItem.js
+++ test/GameItem.js
@@ -1,11 +1,11 @@
-const GameItem = artifacts.require('GameItem');
-const GameStore = artifacts.require('GameStore');
+const GameItem = artifacts.require("GameItem");
+const GameStore = artifacts.require("GameStore");
 // const MockGameStore = artifacts.require('MockGameStore');
-const GamePlayer = artifacts.require('GamePlayer');
+const GamePlayer = artifacts.require("GamePlayer");
 
-const utils = require('web3-utils');
+const utils = require("web3-utils");
 assert = {
-  ...require('truffle-assertions'),
+  ...require("truffle-assertions"),
   ...assert
 };
 
@@ -16,9 +16,13 @@ const ItemGameState = {
 
 async function assertEnum(expected, actual, message) {
   if (actual === undefined) {
-    assert.fail(message);    
+    assert.fail(message);
   }
-  assert.equal(expected, actual, `Expected enum [${expected}] == [${actual}] ${message}`);
+  assert.equal(
+    expected,
+    actual,
+    `Expected enum [${expected}] == [${actual}] ${message}`
+  );
 }
 
 async function get(item, prop, isnum) {
@@ -30,96 +34,161 @@ async function get(item, prop, isnum) {
   }
 }
 
-contract('GamePlayer  ', (accounts) => {
+contract("GamePlayer  ", accounts => {
   let store;
   let owner;
   let player;
   let gameAccounts;
 
   before(async () => {
-
-    console.log('creating gamestore');
+    console.log("creating gamestore");
     store = await GameStore.new();
-    player = await GamePlayer.new('playa', store.address);
+    player = await GamePlayer.new("playa", store.address);
     await store.RegisterAccount(accounts[0]);
     gameAccounts = await store.GetAllAccounts();
-    console.log('accounts', gameAccounts);
-    assert.equal(gameAccounts[0], accounts[0], "should have a game account registered");
+    console.log("accounts", gameAccounts);
+    assert.equal(
+      gameAccounts[0],
+      accounts[0],
+      "should have a game account registered"
+    );
 
     console.log(store.address);
     owner = accounts[0];
-    console.log('owner', owner);
-    assert.notEqual(owner, '0x0000000000000000000000000000000000000000');
+    console.log("owner", owner);
+    assert.notEqual(owner, "0x0000000000000000000000000000000000000000");
   });
 
-  it('ANYONE:can get game item props', async () => {
-    const item = await GameItem.new(store.address, 'item1', 'weapon', '{game:data}');
+  it("ANYONE:can get game item props", async () => {
+    const item = await GameItem.new(
+      store.address,
+      "item1",
+      "weapon",
+      "{game:data}"
+    );
     const props = await item.GetProps.call();
-    
-    assert.strictEqual(utils.hexToNumber(props.gameState), ItemGameState.NotRegistered, 'expected not registred');
-    assert.strictEqual(props.owner, owner, 'expected owner');
-    assert.strictEqual(utils.hexToNumber(props.price), 0, 'expected price');
-    assert.strictEqual(props.game, store.address, 'expected game address');
-    assert.strictEqual(props.itemKey, 'item1', 'item key');
-    assert.strictEqual(props.itemType, 'weapon', 'item type');
-  });
 
+    assert.strictEqual(
+      utils.hexToNumber(props.gameState),
+      ItemGameState.NotRegistered,
+      "expected not registred"
+    );
+    assert.strictEqual(props.owner, owner, "expected owner");
+    assert.strictEqual(utils.hexToNumber(props.price), 0, "expected price");
+    assert.strictEqual(props.game, store.address, "expected game address");
+    assert.strictEqual(props.itemKey, "item1", "item key");
+    assert.strictEqual(props.itemType, "weapon", "item type");
+  });
 
   // OWNER change owner
-  it('OWNER:ChangeOwner:succeed', async () => {
-    const item = await GameItem.new(accounts[1], 'item1', 'weapon', '{game:data}');
+  it("OWNER:ChangeOwner:succeed", async () => {
+    const item = await GameItem.new(
+      accounts[1],
+      "item1",
+      "weapon",
+      "{game:data}"
+    );
     await item.ChangeOwner(accounts[2]);
-    assert.equal(accounts[2], await get(item, 'owner'), 'Owner');
+    assert.equal(accounts[2], await get(item, "owner"), "Owner");
   });
 
-  it('NOT-OWNER:ChangeOwner:revert - not the owner', async () => {
-    const item = await GameItem.new(store.address, 'item1', 'weapon', '{game:data}');
-    await assert.reverts(item.ChangeOwner(accounts[1], { from: accounts[2] }), "Must be owner");
+  it("NOT-OWNER:ChangeOwner:revert - not the owner", async () => {
+    const item = await GameItem.new(
+      store.address,
+      "item1",
+      "weapon",
+      "{game:data}"
+    );
+    await assert.reverts(
+      item.ChangeOwner(accounts[1], { from: accounts[2] }),
+      "Must be owner"
+    );
   });
-
 
   // OWNER change price
-  it('OWNER:SetPrice:succeed', async () => {
-    const item = await GameItem.new(store.address, 'item1', 'weapon', '{game:data}');
+  it("OWNER:SetPrice:succeed", async () => {
+    const item = await GameItem.new(
+      store.address,
+      "item1",
+      "weapon",
+      "{game:data}"
+    );
     await item.SetPrice(200);
-    assert.equal(200, await get(item, 'price'), 'Price');
+    assert.equal(200, await get(item, "price"), "Price");
   });
-  it('NOT-OWNER:SetPrice:revert - not the owner', async () => {
-    const item = await GameItem.new(store.address, 'item1', 'weapon', '{game:data}');
-    await assert.reverts(item.SetPrice(200, { from: accounts[2] }), "Must be owner");
+  it("NOT-OWNER:SetPrice:revert - not the owner", async () => {
+    const item = await GameItem.new(
+      store.address,
+      "item1",
+      "weapon",
+      "{game:data}"
+    );
+    await assert.reverts(
+      item.SetPrice(200, { from: accounts[2] }),
+      "Must be owner"
+    );
   });
 
   //game
-  it('Game:GameChangeOwner:succeed', async () => {
-    const item = await GameItem.new(accounts[1], 'item1', 'weapon', '{game:data}');
+  it("Game:GameChangeOwner:succeed", async () => {
+    const item = await GameItem.new(
+      accounts[1],
+      "item1",
+      "weapon",
+      "{game:data}"
+    );
     await item.GameChangeOwner(accounts[2], { from: accounts[1] });
-    assert.equal(accounts[2], await get(item, 'owner'), 'Owner');
+    assert.equal(accounts[2], await get(item, "owner"), "Owner");
   });
-  it('Game:GameChangeOwner:revert - not the game', async () => {
-    const item = await GameItem.new(accounts[1], 'item1', 'weapon', '{game:data}');
-    await assert.reverts(item.GameChangeOwner(accounts[3], { from: accounts[2] }), "Must be the GameStore");
+  it("Game:GameChangeOwner:revert - not the game", async () => {
+    const item = await GameItem.new(
+      accounts[1],
+      "item1",
+      "weapon",
+      "{game:data}"
+    );
+    await assert.reverts(
+      item.GameChangeOwner(accounts[3], { from: accounts[2] }),
+      "Must be the GameStore"
+    );
   });
 
-  it('Game:GameItemRegistered:succeed', async () => {
-    const item = await GameItem.new(accounts[1], 'item1', 'weapon', '{game:data}');
+  it("Game:GameItemRegistered:succeed", async () => {
+    const item = await GameItem.new(
+      accounts[1],
+      "item1",
+      "weapon",
+      "{game:data}"
+    );
     await item.GameItemRegistered({ from: accounts[1] });
-    assertEnum(ItemGameState.Registered, await get(item, 'gameState', true), 'Registered')
+    assertEnum(
+      ItemGameState.Registered,
+      await get(item, "gameState", true),
+      "Registered"
+    );
   });
-  it('Game:GameItemRegistered:revert - not the game', async () => {
-    const item = await GameItem.new(accounts[1], 'item1', 'weapon', '{game:data}');
-    await assert.reverts(item.GameItemRegistered({ from: accounts[2] }), "Must be the GameStore");
+  it("Game:GameItemRegistered:revert - not the game", async () => {
+    const item = await GameItem.new(
+      accounts[1],
+      "item1",
+      "weapon",
+      "{game:data}"
+    );
+    await assert.reverts(
+      item.GameItemRegistered({ from: accounts[2] }),
+      "Must be the GameStore"
+    );
   });
 
-
-    // //change owners when owned
-    // await item.ChangeOwner(accounts[1]);
-    // //a
-    // await mockStore.RegisterItemForSale(item.address);
-    // assert.equal(ItemGameState.Owned, await get(item, 'gameState', true), 'Owned');
-    // await item.ChangeOwner(accounts[2], { from: accounts[1]});                                                                                                       
-    // await item.MakeOffer(20, { from: accounts[2] });
-    // assert.equal(ItemGameState.ForSale, await get(item, 'gameState', true), 'ForSale');
-    // await item.ChangeOwner(accounts[0], { from: store.address });
+  // //change owners when owned
+  // await item.ChangeOwner(accounts[1]);
+  // //a
+  // await mockStore.RegisterItemForSale(item.address);
+  // assert.equal(ItemGameState.Owned, await get(item, 'gameState', true), 'Owned');
+  // await item.ChangeOwner(accounts[2], { from: accounts[1]});
+  // await item.MakeOffer(20, { from: accounts[2] });
+  // assert.equal(ItemGameState.ForSale, await get(item, 'gameState', true), 'ForSale');
+  // await item.ChangeOwner(accounts[0], { from: store.address });
   //});
 
   // it('player can store and discard items', async () => {
@@ -153,6 +222,4 @@ contract('GamePlayer  ', (accounts) => {
   //   assert.strictEqual(items[0], item.address, 'expected item 1 address');
   //   assert.strictEqual(items[1], item2.address, 'expected item 2 address');
   // });
-
-
 });
